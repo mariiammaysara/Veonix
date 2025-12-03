e22dd99e2da8d110b6092e7bfff598714fe1a58d
Mariam Maysara <mariammaysara.ai@gmail.com>
2025-12-03 13:14:39 +0200

    fix import path for vercel

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 import sys
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 
 from app.main import app
+
 from mangum import Mangum
 
 handler = Mangum(app)
