7557c1c2c8564d32d36ef5faf97a8bd16eb86cd9
Mariam Maysara <mariammaysara.ai@gmail.com>
2025-12-03 13:04:02 +0200

    fix folder structure for vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend"))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(BASE_DIR)
 
 from app.main import app
