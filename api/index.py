8e8450cc9d0428f7df233aec37c5c9f5f1feb84b
Mariam Maysara <mariammaysara.ai@gmail.com>
2025-12-03 13:18:11 +0200

    fix vercel handler

--- api/index.py
+++ api/index.py
@@ -1,11 +1,4 @@
-import sys
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(BASE_DIR)
-
-from app.main import app
-
 from mangum import Mangum
+from app.main import app
 
 handler = Mangum(app)
